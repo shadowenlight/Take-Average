3a3bbe33da20aa20755a2e7ce80898ce751d1293
shadowenlight <isikomer98@gmail.com>
2023-11-09 00:13:59 +0300

    ReadIntegerTakeAvg

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.WriteLine("Enter a number of integers seperated by spaces:\n");
+
+            string input = Console.ReadLine();
+
+            string[] strings = input.Split(' ');
+
+            int sum = 0;
+            foreach (var item in strings)
+            {
+                int number = int.Parse(item);
+
+                sum += number;
+            }
+            int average = sum / strings.Length;
+
+            Console.WriteLine("Average of the integers is: " + average);
+        }
+    }
+}
